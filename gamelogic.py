8c676bc15a45a8e1d56bbf2864beb0724e0301f0
Ammar Mohamed Amanulla <zpac577@live.rhul.ac.uk>
2026-08-22 10:51:36 +0100

    Added simple game mechanic

--- gamelogic.py
+++ gamelogic.py
@@ -1,3 +1,10 @@
 Cards = {
     "J":3, "9":2, "A":1, "10":1, "K":0, "Q":0, "8":0, "7":0 
 }
+In_game = []
+x = 0
+for i in range(0,4):
+     x = input("Place your card in hand: ")
+     In_game.append(x)
+print([In_game])
+
