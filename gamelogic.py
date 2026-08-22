606ec7230de02d6de0823ef961c78c763871b7b6
Ammar Mohamed Amanulla <zpac577@live.rhul.ac.uk>
2026-08-22 11:12:27 +0100

    Added extra code

--- gamelogic.py
+++ gamelogic.py
@@ -2,10 +2,10 @@ Cards = {
     "J":3, "9":2, "A":1, "10":1, "K":0, "Q":0, "8":0, "7":0 
 }
 In_game = []
-x = 0
 for i in range(0,4):
-     while x not in Cards:
-        x = input("Place your card in hand: ")
-        In_game.append(x)
+    x = input("Place your card in hand: ")
+    while x not in Cards:
+        x = input("Invalid card. Please place your card in hand: ")
+    In_game.append(x)
 print([In_game])
 
